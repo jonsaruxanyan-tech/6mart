e3fdd8392cbbe80879bb790759b7b791488ce95c
jonsaruxanyan-tech <jonsaruxanyan@gmail.com>
2026-03-06 10:55:52 +0400

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// using namespace std allows using standard library names directly
+using namespace std; 
+
+int main() {
+  cout << "Hello World!"; // cout is used for output, followed by <<
+  return 0; 
+}
